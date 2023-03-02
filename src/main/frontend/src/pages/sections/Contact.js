f08a62d861c8ca5f186539816621d996436e5d3d
seoyoonho <gogoy2643@naver.com>
2023-03-02 17:46:39 +0900

    연락처 화면 완성

--- src/main/frontend/src/pages/sections/Contact.js
+++ src/main/frontend/src/pages/sections/Contact.js
@@ -26,40 +26,62 @@ function Contact() {
     return (
         <>
             <div className="section section-tabs">
-                <Container className="text-center">
-                    <Row>
+                <Container className="text-center container-flex">
+                    <Row className="row-flex">
                         <Col className="ml-auto mr-auto">
-                            <div className="typography-line-flex">
-                                <h5> 신랑에게 연락하기 </h5>
-                                <FontAwesomeIcon icon={faPhone} size="2x"/>
-                                <FontAwesomeIcon icon={faCommentSms} size="2x"/>
+                            <div className="typography-line-flex-row">
+                                <h3 className="contact-icon-style" style={{flex:2}}>신랑에게 연락하기</h3>
+                                <div className="contact-icon-style">
+                                    <FontAwesomeIcon icon={faPhone} size="2x"/>
+                                    <FontAwesomeIcon icon={faCommentSms} size="2x"/>
+                                </div>
                             </div>
-                            <div className="typography-line">
-                                <h5> 신부에게 연락하기 </h5>
+                            <div className="typography-line-flex-row">
+                                <h3 className="contact-icon-style" style={{flex:2}}>신부에게 연락하기</h3>
+                                <div className="contact-icon-style">
+                                    <FontAwesomeIcon icon={faPhone} size="2x"/>
+                                    <FontAwesomeIcon icon={faCommentSms} size="2x"/>
+                                </div>
                             </div>
                         </Col>
                     </Row>
-                    <Row>
+                    <Row className="row-flex">
                         <Col className="ml-auto mr-auto">
-                            <div className="typography-line">
+                            <div className="typography-line-flex-col">
                                 <h5> 신랑측 혼주 </h5>
                             </div>
-                            <div className="typography-line">
+                            <div className="typography-line-flex-col">
                                 <h5> 아버지 아무개 </h5>
+                                <div className="contact-icon-style">
+                                    <FontAwesomeIcon icon={faPhone} size="2x"/>
+                                    <FontAwesomeIcon icon={faCommentSms} size="2x"/>
+                                </div>
                             </div>
-                            <div className="typography-line">
+                            <div className="typography-line-flex-col">
                                 <h5> 어머니 누구게 </h5>
+                                <div className="contact-icon-style">
+                                    <FontAwesomeIcon icon={faPhone} size="2x"/>
+                                    <FontAwesomeIcon icon={faCommentSms} size="2x"/>
+                                </div>
                             </div>
                         </Col>
                         <Col className="ml-auto mr-auto">
-                            <div className="typography-line">
+                            <div className="typography-line-flex-col">
                                 <h5> 신부측 혼주 </h5>
                             </div>
-                            <div className="typography-line">
+                            <div className="typography-line-flex-col">
                                 <h5> 아버지 아무개 </h5>
+                                <div className="contact-icon-style">
+                                    <FontAwesomeIcon icon={faPhone} size="2x"/>
+                                    <FontAwesomeIcon icon={faCommentSms} size="2x"/>
+                                </div>
                             </div>
-                            <div className="typography-line">
+                            <div className="typography-line-flex-col">
                                 <h5> 어머니 누구게 </h5>
+                                <div className="contact-icon-style">
+                                    <FontAwesomeIcon icon={faPhone} size="2x"/>
+                                    <FontAwesomeIcon icon={faCommentSms} size="2x"/>
+                                </div>
                             </div>
                         </Col>
                     </Row>
